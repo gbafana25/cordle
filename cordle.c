38b192fdcbab26f991bf0a1c44dee885f95ec90d
gbafana25 <garethkm25@gmail.com>
2022-02-27 11:16:25 -0600

    letter search/higlighting done, find wordlist with without repeating letters, fix weird printing issues

--- cordle.c
+++ cordle.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define FILE_LENGTH 85166
 
@@ -11,7 +12,7 @@ char *get_random_word() {
 	size_t size = 0;
 	//char final[6];
 	char *final = malloc(sizeof(char) * 6);
-	srand(time(NULL));
+	srand(time(0));
 	int random = (rand()%(FILE_LENGTH/2));
 	int c = 0;
 	while(getline(&l, &size, w) != -1) {
@@ -28,6 +29,31 @@ char *get_random_word() {
 	final[6] = '\0';
 	return final;
 		
+
+}
+
+void show_progress(char *answer, char t[5]) {
+
+	for(int i = 0; i < strlen(t); i++) {
+		for(int j = 0; j < strlen(answer); j++) {
+			if(t[i] == answer[j]) {
+				//printf("found letter\n");
+				if(i == j) {
+					write(STDOUT_FILENO, "\e[0;30;42m", 11);
+					break;
+				} else {
+					write(STDOUT_FILENO, "\e[0;30;43m", 11);
+					break;
+				}
+			} else {
+				write(STDOUT_FILENO, "\e[0;30;47m", 11);
+			}
+		}
+		write(STDOUT_FILENO, (void *) &t[i], 1);
+		write(STDOUT_FILENO, "\e[0m", 5);
+		
+	}
+	printf("\n");
 
 }
 
@@ -55,6 +81,7 @@ int main() {
 				printf("You win\n");
 				break;
 			} else {
+				show_progress(ran, guess);
 				turn++;
 			}
 
